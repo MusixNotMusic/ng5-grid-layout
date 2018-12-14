25ee0f11fd5c394184611ce996ff464d6c0b8ca8
AOrangeOrange <aorangeaorange@sina.com>
2018-12-14 11:20:02 +0800

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
-
+import { OnChangeComponent } from './test-component/onChange.component';
+import { NgGridItemComponent } from './component/NgGridItem.component';
+import { NgGridLayoutComponent } from './component/NgGridLayout.component';
+import { NgGridLayoutDirective } from './directives/layout';
+import { EventService } from './service/event.service';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OnChangeComponent,
+    NgGridItemComponent,
+    NgGridLayoutComponent,
+    NgGridLayoutDirective
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
-  providers: [],
+  providers: [EventService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
